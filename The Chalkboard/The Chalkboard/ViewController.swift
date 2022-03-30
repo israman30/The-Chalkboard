92f0fef1e06b7009c4f8ab6c37862c97ddded67e
israman <israelmanzo814@gmail.com>
2022-03-29 22:52:22 -0400

    setting up prject

--- The Chalkboard/The Chalkboard/ViewController.swift
+++ The Chalkboard/The Chalkboard/ViewController.swift
@@ -7,13 +7,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainController: UIViewController {
+    
+    let tableView: UITableView = {
+        let tv = UITableView()
+        return tv
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "The Chalkboard"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(add))
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(tableView)
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
     }
-
+    @objc func add() {
+        print("Added")
+    }
 
 }
 
