7800e388acadef6a7348a21c4a636d7b31536031
israman <israelmanzo814@gmail.com>
2022-03-31 22:43:59 -0400

    added button setup + textifield setup + color extension

--- The Chalkboard/The Chalkboard/MainController.swift
+++ The Chalkboard/The Chalkboard/MainController.swift
@@ -7,6 +7,12 @@
 
 import UIKit
 
+extension UIColor {
+    static var greenColor = UIColor(red: 77/255, green: 125/255, blue: 90/255, alpha: 1)
+}
+
+
+
 class MainController: UIViewController {
     
     let tableView: UITableView = {
@@ -24,9 +30,11 @@ class MainController: UIViewController {
     
     let addButton: UIButton = {
         let btn = UIButton(type: .system)
-        btn.setTitleColor(UIColor.label, for: .normal)
+        btn.setTitleColor(UIColor.systemGray, for: .normal)
+        btn.titleLabel?.font = UIFont(name: "GillSans-Italic", size: 20)
         btn.backgroundColor = .systemGray4
         btn.isEnabled = false
+        btn.layer.cornerRadius = 2
         return btn
     }()
     
@@ -40,7 +48,12 @@ class MainController: UIViewController {
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(openInput))
         navigationItem.rightBarButtonItem?.tintColor = .label
+        addButton.addTarget(self, action: #selector(add), for: .touchUpInside)
+        textField.addTarget(self, action: #selector(input), for: .editingChanged)
         tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.delegate = self
+        tableView.dataSource = self
         
         let stackView = UIStackView(arrangedSubviews: [textField, addButton])
         stackView.distribution = .fillProportionally
@@ -54,9 +67,9 @@ class MainController: UIViewController {
         
         inputHeightConstrain = stackView.heightAnchor.constraint(equalToConstant: inputHeightConstrain?.constant ?? 0.0)
         stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
-        stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-        addButton.setTitle("", for: .normal)
+        stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10).isActive = true
+        
         inputHeightConstrain?.isActive = true
         NSLayoutConstraint.activate([
             tableView.topAnchor.constraint(equalTo: stackView.bottomAnchor),
@@ -64,12 +77,27 @@ class MainController: UIViewController {
             tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
             tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
         ])
-        
+    }
+    
+    @objc func input() {
+        guard let inputText = textField.text else { return }
+        if !inputText.isEmpty {
+            addButton.isEnabled = true
+            addButton.setTitleColor(UIColor.white, for: .normal)
+            addButton.backgroundColor = .greenColor
+        } else {
+            addButton.isEnabled = false
+            addButton.setTitleColor(UIColor.systemGray, for: .normal)
+            addButton.backgroundColor = .systemGray3
+        }
+    }
+    
+    @objc func add() {
+        print(textField.text ?? "")
     }
     
     @objc func openInput() {
-        print("Opened")
-        if isOpen == false {
+        if !isOpen {
             isOpen = true
             inputHeightConstrain?.constant = 50.0
             addButton.setTitle("Add", for: .normal)
@@ -84,4 +112,20 @@ class MainController: UIViewController {
     }
 
 }
-
+extension MainController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Hello"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, estimatedHeightForFooterInSection section: Int) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+    
+}
