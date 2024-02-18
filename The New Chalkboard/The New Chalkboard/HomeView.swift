34b641b305a595bbb916c0c2dfceb65b158e6708
@israman30 <you@example.com>
2024-02-17 21:46:10 -0500

    F -  added tag model

--- The New Chalkboard/The New Chalkboard/HomeView.swift
+++ The New Chalkboard/The New Chalkboard/HomeView.swift
@@ -7,6 +7,12 @@
 
 import SwiftUI
 
+struct TagModel: Identifiable, Hashable {
+    var id = UUID()
+    var name: String
+    var size: CGFloat = 0
+}
+
 struct HomeView: View {
     
     @State var text = ""
@@ -23,19 +29,27 @@ struct HomeView: View {
                     HStack {
                         Spacer()
                         Button {
-                            
+                            self.addTag()
                         } label: {
                             Text("Add")
                         }
+                        .buttonStyle(.borderedProminent)
                     }
                     
                 }
-                
                 Text("bottom")
                     .padding()
-                    .background(Color.yellow)
+                    .background(Color(.systemGray4))
+                    .foregroundColor(Color(.label))
+                ForEach(tagGroup, id: \.self) { item in
+                    HStack {
+                        Text(item)
+                    }
+                }
+                
                 Spacer()
             }
+            .padding(.horizontal, 5)
             .navigationTitle("The New Chalkboard")
         }
         
@@ -43,6 +57,7 @@ struct HomeView: View {
     
     func addTag() {
         tagGroup.append(text)
+        text = ""
     }
 }
 
