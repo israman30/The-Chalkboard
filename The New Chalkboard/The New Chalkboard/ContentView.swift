af61ee4c6b8c5fef86f5c9c7225e91dcbdfc8c4e
@israman30 <you@example.com>
2024-02-17 20:23:48 -0500

    new project

---
+++ The New Chalkboard/The New Chalkboard/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  The New Chalkboard
+//
+//  Created by Israel Manzo on 2/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        MainView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ The New Chalkboard/The New Chalkboard/The_New_ChalkboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_New_ChalkboardApp.swift
+//  The New Chalkboard
+//
+//  Created by Israel Manzo on 2/17/24.
+//
+
+import SwiftUI
+
+@main
+struct The_New_ChalkboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
