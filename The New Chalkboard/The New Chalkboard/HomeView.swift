88da3081a74a5d3c172fe9f7006d25295f53ca6c
@israman30 <you@example.com>
2024-02-17 22:24:31 -0500

    Fgit add . - added method to add tag and display

--- The New Chalkboard/The New Chalkboard/HomeView.swift
+++ The New Chalkboard/The New Chalkboard/HomeView.swift
@@ -8,58 +8,146 @@
 import SwiftUI
 
 struct TagModel: Identifiable, Hashable {
-    var id = UUID()
+    var id = UUID().uuidString
     var name: String
     var size: CGFloat = 0
 }
 
-struct HomeView: View {
+extension UIScreen {
+    static let screenWidth = UIScreen.main.bounds.width
+}
+
+extension String {
+    func getSize() -> CGFloat {
+        let font = UIFont.systemFont(ofSize: 16)
+        let attributes = [NSAttributedString.Key.font: font]
+        let size = (self as NSString).size(withAttributes: attributes)
+        return size.width
+    }
+}
+
+class TagsViewModel: ObservableObject {
     
-    @State var text = ""
-    @State var tagGroup = [String]()
+    @Published var rows: [[TagModel]] = []
+    @Published var tags: [TagModel] = []
+    @Published var tagInputText = ""
     
-    var body: some View {
-        NavigationView {
-            VStack {
-                VStack {
-                    TextField("Enter something..", text: $text)
-                        .padding()
-                        .font(.title)
-                        .border(Color.black)
-                    HStack {
-                        Spacer()
-                        Button {
-                            self.addTag()
-                        } label: {
-                            Text("Add")
-                        }
-                        .buttonStyle(.borderedProminent)
-                    }
-                    
-                }
-                Text("bottom")
-                    .padding()
-                    .background(Color(.systemGray4))
-                    .foregroundColor(Color(.label))
-                ForEach(tagGroup, id: \.self) { item in
-                    HStack {
-                        Text(item)
-                    }
-                }
-                
-                Spacer()
-            }
-            .padding(.horizontal, 5)
-            .navigationTitle("The New Chalkboard")
-        }
+    init() {
+        tags = [
+            TagModel(name: "Xcode"),
+            TagModel(name: "Android Studio"),
+            TagModel(name: "VS Code"),
+            TagModel(name: "Jetbrains")
+        ]
+        getTags()
+    }
+    
+    func getTags() {
+        var rows: [[TagModel]] = []
+        var currentRow: [TagModel] = []
+        var totalWidth: CGFloat = 0
+        let screenWidth = UIScreen.screenWidth - 10
+        let tagSpacing: CGFloat = 14 + 30 + 6 + 6
         
+        if !tags.isEmpty {
+            for index in  0..<tags.count {
+                tags[index].size = tags[index].name.getSize()
+            }
+            
+            tags.forEach { tag in
+                totalWidth += (tag.size + tagSpacing)
+                
+                if totalWidth > screenWidth{
+                    totalWidth = (tag.size + tagSpacing)
+                    rows.append(currentRow)
+                    currentRow.removeAll()
+                    currentRow.append(tag)
+                } else {
+                    currentRow.append(tag)
+                }
+            }
+            
+            if !currentRow.isEmpty {
+                rows.append(currentRow)
+                currentRow.removeAll()
+            }
+            
+            self.rows = rows
+        } else {
+            rows = []
+        }
     }
     
     func addTag() {
-        tagGroup.append(text)
-        text = ""
+        tags.append(TagModel(name: tagInputText))
+        tagInputText = ""
+        getTags()
+    }
+    
+    func removeTag(by id: String){
+        tags = tags.filter { $0.id != id }
+        getTags()
     }
 }
+
+struct HomeView: View {
+    
+    @StateObject var viewModel = TagsViewModel()
+    
+    var body: some View {
+        VStack{
+            Text("Enter Tag Here:")
+                .padding()
+            TextField("Enter tag", text: $viewModel.tagInputText, onCommit: {
+                viewModel.addTag()
+                viewModel.tagInputText = ""
+            })
+            .padding()
+            .overlay(
+                RoundedRectangle(cornerRadius: 10)
+                    .strokeBorder()
+                    .foregroundColor(.black)
+            )
+            .padding()
+            
+            VStack(alignment: .leading, spacing: 4){
+                ForEach(viewModel.rows, id:\.self) { rows in
+                    HStack(spacing: 6){
+                        ForEach(rows) { row in
+                            
+                            Text(row.name)
+                                .font(.system(size: 16))
+                                .padding(.leading, 14)
+                                .padding(.trailing, 30)
+                                .padding(.vertical, 8)
+                                .background(
+                                    ZStack(alignment: .trailing) {
+                                        Capsule()
+                                            .fill(.gray.opacity(0.3))
+                                        Button{
+                                            viewModel.removeTag(by: row.id)
+                                            viewModel.tagInputText = ""
+                                        } label:{
+                                            Image(systemName: "xmark")
+                                                .frame(width: 15, height: 15)
+                                                .padding(.trailing, 8)
+                                                .foregroundColor(.red)
+                                        }
+                                    }
+                                )
+                        }
+                    }
+                    .frame(height: 28)
+                    .padding(.bottom, 10)
+                }
+            }
+            .padding(24)
+            
+            Spacer()
+        }
+    }
+}
+
 
 #Preview {
     HomeView()
